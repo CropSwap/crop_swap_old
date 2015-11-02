98b32f1e4a725291f2e8de08f280d7d7b2ecc0d9
Ru-T <rwajnberg@gmail.com>
2015-11-02 15:56:43 -0500

    Add associations on to crop and user models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
+  has_many :crops
   # has_attached_file :profile_pic
   # validates_attachment_content_type :profile_pic, content_type: /\Aimage\/.*/
 end
