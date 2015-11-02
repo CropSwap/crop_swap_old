79f8e94c16e44b956d0bd53d1fdf78414e2bd334
Ru-T <rwajnberg@gmail.com>
2015-11-02 14:07:44 -0500

    Configure controller action for creating new user and updating his/her profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,27 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
   # POST /users
   # POST /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      render json:@user
+    elsif @user.errors
+      render json: {error: {code: 400, server_message: @user.errors}}, status: :bad_request
+    else
+      render json: {error: {code: 500, message: "Could not save user", server_message: @user.errors}}, status: :internal_server_error
     end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      render json:@user
+    else
+      format.json { render json: @user.errors, status: :unprocessable_entity }
     end
   end
 
@@ -55,10 +29,28 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
-    end
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  #
+  # # GET /users/new
+  def new
+    @user = User.new
   end
 
   private
