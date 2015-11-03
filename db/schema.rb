128b700883e67b7a77ec206e182287d10db65b72
Ru-T <rwajnberg@gmail.com>
2015-11-03 08:47:18 -0500

    Add indices on foreign keys to crops table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102204113) do
+ActiveRecord::Schema.define(version: 20151103134618) do
 
   create_table "crop_types", force: :cascade do |t|
     t.string   "crop_type"
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20151102204113) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  add_index "crops", ["crop_type_id"], name: "index_crops_on_crop_type_id"
+  add_index "crops", ["user_id"], name: "index_crops_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
